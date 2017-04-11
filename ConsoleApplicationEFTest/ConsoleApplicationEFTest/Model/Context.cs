09acd046f209ab4677380403c5ad66f0a1214ce9
Damian Urbaszewski <durbaszewski@objectivity.co.uk>
2017-04-11 10:25:45 +0200

    App Init

---
+++ ConsoleApplicationEFTest/ConsoleApplicationEFTest/Model/Context.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Z.EntityFramework.Plus;
+
+namespace ConsoleApplicationEFTest.Model
+{
+    public class AppContext : DbContext
+    {
+        internal BaseQueryFilterInterceptor SoftDeletableFilter;
+        public AppContext() : base("name=ContextString")
+        {
+            this.SoftDeletableFilter = this.Filter<TestEntity>(x => x.Where(a => a.Deleted == false));
+        }
+
+        public DbSet<TestEntity> Entities { get; set; }
+    }
+}
---
+++ ConsoleApplicationEFTest/ConsoleApplicationEFTest/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApplicationEFTest.Model;
+
+namespace ConsoleApplicationEFTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InitData();
+            Console.WriteLine("Result with deleted items:");
+            foreach (var name in DisplayWithDeleted())
+            {
+                Console.WriteLine(name);
+            }
+
+            Console.WriteLine("Result without deleted items:");
+            foreach (var name in DisplayWithoutDeleted())
+            {
+                Console.WriteLine(name);
+            }
+            Console.ReadKey();
+        }
+
+        public static List<string> DisplayWithDeleted()
+        {
+            using (var db = new AppContext())
+            {
+                db.SoftDeletableFilter.Disable();
+                return db.Entities.Select(e => e.Name).ToList();
+            }
+        }
+
+        public static List<string> DisplayWithoutDeleted()
+        {
+            using (var db = new AppContext())
+            {
+                db.SoftDeletableFilter.Enable();
+                return db.Entities.Select(e => e.Name).ToList();
+            }
+        }
+
+        public static void InitData()
+        {
+            using (var db = new AppContext())
+            {
+                if (!db.Entities.Any())
+                {
+                    var entityList = new List<TestEntity>
+                    {
+                        new TestEntity
+                        {
+                            Id = Guid.NewGuid(),
+                            Name = "DeletedItem1",
+                            Deleted = true
+                        },
+                        new TestEntity
+                        {
+                            Id = Guid.NewGuid(),
+                            Name = "Item1",
+                            Deleted = false
+                        },
+                        new TestEntity
+                        {
+                            Id = Guid.NewGuid(),
+                            Name = "DeletedItem2",
+                            Deleted = true
+                        },
+                        new TestEntity
+                        {
+                            Id = Guid.NewGuid(),
+                            Name = "Item2",
+                            Deleted = false
+                        }
+                    };
+                    db.Entities.AddRange(entityList);
+                    db.SaveChanges();
+                }
+            }
+        }
+    }
+}
